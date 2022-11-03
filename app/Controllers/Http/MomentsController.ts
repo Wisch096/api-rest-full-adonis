9646ddadb80164d6557a5e7144725289bcb36389
Wisch096 <wisch.pg@outlook.com>
2022-11-03 11:37:38 -0300

    adicionando-imagem

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -1,11 +1,36 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import {v4 as uuidv4} from 'uuid'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Moment from 'App/Models/Moment'
+import Application from "@ioc:Adonis/Core/Application"
 
 export default class MomentsController {
 
-    public async store(){
-        return {
-            msg: 'Deu certo!',
+    private validationOptions = {
+        types: ['image'],
+        size: '2mb',
+    }
+
+    public async store({request, response}: HttpContextContract){
+        const body = request.body()
+        const image = request.file('image', this.validationOptions)
+
+        if(image){
+           const imageName = `${uuidv4()}.${image.extname}`
+
+           image.move(Application.tmpPath('uploads'), {
+            name
+           })
         }
+
+        const moment = await Moment.create(body)
+
+        response.status(201)
+
+        return {
+            message: 'Momento criado com sucesso!',
+            data: moment,
+        }
+ 
     }
 
 }
